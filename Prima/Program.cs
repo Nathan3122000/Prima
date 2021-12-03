2b4f80ec2a2c39c9a5915a3a37cdc8e6101bda0c
Nathanael P.A <Nathanael P.A@LAPTOP-KC1AUE3L>
2021-12-03 10:22:38 +0700

    Add project files.

---
+++ Prima/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Prima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int angka, i, m = 0, j = 0;
+            Console.Write("Masukkan angka: ");
+            angka = Convert.ToInt32(Console.ReadLine());
+            m = angka / 2;
+            for (i = 2; i <= m; i++)
+            {
+                if (angka % i == 0)
+                {
+                    Console.Write("Angka bukan prima");
+                    j = 1;
+                    break;
+                }
+            }
+            if (j == 0)
+                Console.Write("Angka prima.");
+        }
+    }
+}
